dfe2265e1af70198a5738f4ee55a8fd12c2d6d33
Matias Flores <49283383+nanoes@users.noreply.github.com>
2022-07-11 19:27:11 -0300

    useContext

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ThemeContext from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeContext.Provider value="blue">
     <App />
-  </React.StrictMode>
+    </ThemeContext.Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
